f68e6368d0ab926a7c7151f3b93d07167214dfc6
Leon Wei <Leon.wei@tradeshift.com>
2020-06-05 13:44:31 +0800

    Fix import bug

--- src/layouts/basicLayout/basicLayout.js
+++ src/layouts/basicLayout/basicLayout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Layout } from 'antd';
 import Header from '../../components/header/index';
 import Sider from '../../components/sider/index';
-import Content from '../../components/Content/index';
+import Content from '../../components/content/index';
 import style from './basicLayout.less';
 
 export default function BasicLayout() {
